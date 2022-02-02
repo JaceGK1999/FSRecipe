8108105ac889d3235a6635dd192a4d1f7d541759
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-02 10:52:08 -0800

    added a render meals thing

--- utils.js
+++ utils.js
@@ -3,3 +3,8 @@ export function renderIngredientLI(item) {
     li.textContent = `${item.qty} ${item.measurement} of ${item.ingredient} (${item.calories} Calories)`;
     return li;
 }
+export function renderMealLI(meal) {
+    const li = document.createElement('li');
+    li.textContent = `${meal.name} - ${meal.count} Ingredients`;
+    return li;
+}
